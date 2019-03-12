70089b8d929a977f7910f55378d25ad5cd334c81
ZVNexus <zvnexus@outlook.com>
2019-03-12 13:57:56 -0400

    Inherit pixelgapps, if it exists.
    Who needs to flash gapps, when you can just build it with the ROM? :D
    
    Change-Id: I87e1ac1f9b8e05ec29027466ec883f587895da30

--- device.mk
+++ device.mk
@@ -22,6 +22,7 @@
 #
 
 $(call inherit-product, vendor/oneplus/oneplus3/oneplus3-vendor.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
 $(call inherit-product, vendor/statix/config/dalvik/phone-xxhdpi-4096-dalvik-heap.mk)
 $(call inherit-product, vendor/statix/config/dalvik/phone-xxhdpi-2048-hwui-memory.mk)
 $(call inherit-product, device/oneplus/oneplus3/hidl/hidl.mk)
